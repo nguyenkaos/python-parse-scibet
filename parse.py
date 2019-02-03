9b1d7601312f7c4208f3186c9e7673dc264faccf
pinkLotus <nguyen.caal@gmail.com>
2019-02-03 12:26:41 +0700

    Create parse.py

---
+++ parse.py
@@ -0,0 +1,17 @@
+
+
+clase Scibet:
+    @staticmethod
+    def get_html(url):
+        pass
+
+    @staticmethod
+    def get_data_from_html(html):
+        pass
+
+    @def get_data_one_day(date_str):
+        url = ''
+        html = Scibet.get_html(url)
+        data = Scibet.get_data_from_html(html)
+        return data
+
