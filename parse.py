d9a6afc6435aa0b9020331f1c7f9fa2a24a38180
pinkLotus <nguyen.caal@gmail.com>
2019-02-03 12:27:01 +0700

    Update parse.py

--- parse.py
+++ parse.py
@@ -9,7 +9,8 @@ clase Scibet:
     def get_data_from_html(html):
         pass
 
-    @def get_data_one_day(date_str):
+    @staticmethod
+    def get_data_one_day(date_str):
         url = ''
         html = Scibet.get_html(url)
         data = Scibet.get_data_from_html(html)
